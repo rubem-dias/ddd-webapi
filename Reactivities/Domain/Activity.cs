5939d4eccaee0c6dfc53cac0bf14858c2b7c95a1
rubem-dias <rubemdias65@gmail.com>
2023-03-26 19:07:33 -0300

    (feature) Adding new Activity Domain to project

---
+++ Reactivities/Domain/Activity.cs
@@ -0,0 +1,14 @@
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; } 
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Descriptionm { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Vanue { get; set; }
+    }
+}
