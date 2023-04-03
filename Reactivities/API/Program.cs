ee77be6d73849df4a665f05c68dfdbc14dc4f312
rubem-dias <rubemdias65@gmail.com>
2023-04-03 01:02:43 -0300

    (feature): adding new controller and controllerbase

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -38,7 +38,8 @@ var services = scope.ServiceProvider;
 try 
 {
     var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
 } 
 catch (System.Exception ex) 
 {
