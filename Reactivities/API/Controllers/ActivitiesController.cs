945b9ba971d93c0c67d5d6cc9c293c25d1018490
rubem-dias <rubemdias65@gmail.com>
2023-05-13 02:52:11 -0300

    feat(log): adding ILogger to the projecct

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -8,21 +8,25 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
+        private readonly ILogger<ActivitiesController> _logger;
+        public ActivitiesController(DataContext context, ILogger<ActivitiesController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities() 
         {
+            _logger.LogInformation("Getting all activities from controller activities");
             return await _context.Activities.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivitiesById(Guid id) 
         {
+            _logger.LogInformation($"Getting all activities from controller activities with id: {id}");
             return await _context.Activities.FindAsync(id);
         }
     }
